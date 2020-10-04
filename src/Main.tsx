71c4fc32fcd2541d1e1d98e0d3fbd84bcab4a92f
Nanoha Takamachi <yusukennh@gmail.com>
2020-10-04 13:52:02 +0900

    use images for pieces instead of charactors

--- src/Main.tsx
+++ src/Main.tsx
@@ -8,8 +8,6 @@ import { IState } from './states/IState';
 import { IBoard } from './states/IBoard';
 import { RecordView } from './RecordView'
 
-
-
 const Main: React.FC = () => {
   return (
     <div style={{border: 'solid 1px blue', display: 'flex', height: 600}}>
@@ -24,11 +22,21 @@ const Main: React.FC = () => {
   )
 }
 
+const pieceToImg = (p: Piece) => {
+  if(p === Piece.EMPTY) return <div/>
+  else {
+    const pieceArr = ['FU', 'KY', 'KE', 'GI', 'KI', 'KA', 'HI', 'OU', 'TO', 'NY', 'NK', 'NG', 'UM', 'RY']
+    const isW = isWhite(p)
+    return <img src={`svg/piece/${isW?1:0}${pieceArr[(isW?p-14:p)-1]}.svg`} />
+  }
+}
+
+
 const Board: React.FC = () => {
   const { board, selected, isBlackTurn, bStand, wStand } = useSelector<IState, IBoard>(a => a.board)
 
   const Square: React.FC<{row: number, col: number, p: Piece}> = (props) => (
-    <td style={{border: 'solid 1px', backgroundColor: (props.row*9+props.col)===selected?'#FF0000':''}} key={`square${props.row*9+props.col}`} onClick={() => moveHandler(props.row*9+props.col)}>{pieceChar(props.p)}</td>
+  <td style={{border: 'solid 1px', backgroundColor: (props.row*9+props.col)===selected?'#FF0000':''}} key={`square${props.row*9+props.col}`} onClick={() => moveHandler(props.row*9+props.col)}>{pieceToImg(props.p)}</td>
   )
 
   const fields = [...Array(9).keys()].map(i =>
@@ -53,7 +61,7 @@ const Stand: React.FC<{isBlack: boolean}> = (props) => {
 
   const PieceOnStand: React.FC<{p: Piece, i: number}> = (p) => {
     const nPiece = (props.isBlack?bStand:wStand).filter(q => p.p===q).length
-    if(nPiece) return <td style={{backgroundColor: selected===p.i?'#FF0000':''}} onClick={() => select(p.i)}>{pieceChar(p.p)} x {nPiece}</td>
+    if(nPiece) return <td style={{backgroundColor: selected===p.i?'#FF0000':''}} onClick={() => select(p.i)}>{pieceToImg(p.p)}{nPiece}</td>
     else return <td/>
   }
 
